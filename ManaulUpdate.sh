da488e2f82234d741c40e93bfab83557fd522265
parsdev-com <62644663+parsdev-com@users.noreply.github.com>
2021-01-16 23:57:27 +0330

    Create ManaulUpdate.sh

---
+++ ManaulUpdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -----------------------------------------------
+echo 1 - Directadmin User ID - UID :
+read UID
+echo 2 - Directadmin License ID - LID :
+read LID
+echo -----------------------------------------------
+echo update directadmin core...
+echo -----------------------------------------------
+cd /usr/local/directadmin
+wget --no-check-certificate -O update.tar.gz "https://www.directadmin.com/cgi-bin/daupdate?uid=$UID&lid=$LID"
+tar xvzf update.tar.gz
+./directadmin p
+cd scripts
+./update.sh
+systemctl restart directadmin
