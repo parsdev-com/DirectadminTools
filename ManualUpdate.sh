ca462452afbda7df9066026dca17f499de9ecadd
parsdev-com <62644663+parsdev-com@users.noreply.github.com>
2022-10-05 12:03:30 +0330

    Update ManualUpdate.sh

--- ManualUpdate.sh
+++ ManualUpdate.sh
@@ -1,5 +1,5 @@
 COMMIT=$(dig +short -t txt "current-version.directadmin.com" | sed 's|.*commit=\([0-9a-f]*\).*|\1|')
-wget "https://download.directadmin.com/directadmin_${COMMIT}_linux_amd64.tar.gz" -O "/usr/local/directadmin/update.tar.gz"
+wget "http://download.directadmin.com/directadmin_${COMMIT}_linux_amd64.tar.gz" -O "/usr/local/directadmin/update.tar.gz"
 tar xzf /usr/local/directadmin/update.tar.gz -C /usr/local/directadmin
 /usr/local/directadmin/directadmin p
 /usr/local/directadmin/scripts/update.sh
