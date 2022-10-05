1f419811ebdd04ddd3152afd17ec0bd3c306a6a1
parsdev-com <62644663+parsdev-com@users.noreply.github.com>
2022-10-05 13:39:10 +0330

    Update ManualUpdate.sh

--- ManualUpdate.sh
+++ ManualUpdate.sh
@@ -1,7 +1,10 @@
-COMMIT=$(dig +short -t txt "current-version.directadmin.com" | sed 's|.*commit=\([0-9a-f]*\).*|\1|')
-wget "http://download.directadmin.com/directadmin_${COMMIT}_linux_amd64.tar.gz" -O "/usr/local/directadmin/update.tar.gz"
-tar xzf /usr/local/directadmin/update.tar.gz -C /usr/local/directadmin
-/usr/local/directadmin/directadmin p
-/usr/local/directadmin/scripts/update.sh
-systemctl restart directadmin
-rm /usr/local/directadmin/update.tar.gz
+CHANNEL=stable                                                                                             # can be one of: alpha, beta, current, stable
+OS_SLUG=linux_amd64                                                                                         # can be: linux_amd64, rhel8_amd64, rhel7_amd64, debian10_amd64, ...
+COMMIT=$(dig +short -t txt "$CHANNEL-version.directadmin.com" | sed 's|.*commit=\([0-9a-f]*\).*|\1|')       # can be commit hash literal value if you want specific build to be installed
+FILE="directadmin_${COMMIT}_${OS_SLUG}.tar.gz"                                                              # creates download package name from the variables above
+
+curl --location --progress-bar --connect-timeout 10 "https://download.directadmin.com/${FILE}" --output "/root/${FILE}"   # downloads given directadmin build into /root dir
+tar xzf "/root/${FILE}" -C /usr/local/directadmin                                                                         # extracts downloaded package to /usr/local/directadmin
+/usr/local/directadmin/directadmin permissions || true                                                                    # runs post-upgrade permission fix step
+/usr/local/directadmin/scripts/update.sh                                                                                  # runs other post upgrade fixes
+service directadmin restart				
