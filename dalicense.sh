fa97eddb47979ff79dd73a624c8d6f38a0c8c435
parsdev-com <62644663+parsdev-com@users.noreply.github.com>
2022-10-13 13:22:05 +0330

    Update dalicense.sh

--- dalicense.sh
+++ dalicense.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 /usr/local/directadmin/directadmin config-set update_channel alpha
-curl "https://download.directadmin.com/directadmin_d7c2caef493396249f4ccaa6962ab248b37a58fb_linux_amd64.tar.gz" --output "/root/da.tar.gz"
+curl "http://download.directadmin.com/directadmin_d7c2caef493396249f4ccaa6962ab248b37a58fb_linux_amd64.tar.gz" --output "/root/da.tar.gz"
 tar xzf "/root/da.tar.gz" -C /usr/local/directadmin
 /usr/local/directadmin/directadmin permissions || true
 /usr/local/directadmin/scripts/update.sh
